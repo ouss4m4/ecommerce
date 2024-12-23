e5089ff61110b5602b2736edaa95a0d55aef7921
Oussama <oussama@notifyai.io>
2024-12-23 20:22:46 +0400

    extra helpers to separate files

--- src/lib/fetchImageAndInsertInDbAsync.ts
+++ src/lib/fetchImageAndInsertInDbAsync.ts
@@ -1,20 +1,22 @@
 import { pipeline } from 'stream/promises';
 import { createWriteStream } from 'fs';
 
-export const downloadExternalImageAndSaveIt = async (url: string, filePath: string): Promise<string | Error> => {
-  console.log('downloading ', url);
+export const downloadExternalImageAndSaveToDisk = async (url: string, filePath: string): Promise<string | Error> => {
   let response = await fetch(url);
   if (!response.ok || !response.body) {
-    console.error('wtf');
-    throw new Error('Failed to fetch Image ' + filePath);
+    throw new Error('Failed to fetch Image ' + url);
   }
 
   try {
+    // check storage folder (repetetive... omit for now)
+    // await mkdir(storageFolder, { recursive: true });
+
+    // extract filename (image)
     let writer = createWriteStream(filePath);
     await pipeline(response.body as any, writer);
     return filePath;
   } catch (error) {
-    let message = 'Error while saving image ' + filePath;
+    let message = 'Error while saving image ' + url;
 
     if (error instanceof Error) {
       message = error.message;
---
+++ src/lib/fetchImageAndSaveProductTask.ts
@@ -0,0 +1,33 @@
+import { join } from 'path';
+import { downloadExternalImageAndSaveToDisk } from './fetchImageAndInsertInDbAsync';
+import { AppDataSource } from '../db/setupDb';
+import { Product } from '../entities/product.entity';
+
+export const fetchImageAndInsertInDbAsync = async (
+  sku: string,
+  name: string,
+  description: string,
+  category: string,
+  price: number,
+  imageUrl: string
+): Promise<boolean | Error> => {
+  const productRepo = AppDataSource.getRepository(Product);
+  try {
+    let filePath = join(__dirname, '..', 'storage', `${sku}.png`);
+    await downloadExternalImageAndSaveToDisk(imageUrl, filePath);
+    // later on delegate this to be the return value of the promise.
+    // and insert items at once
+    await productRepo.insert({
+      name,
+      description,
+      category,
+      price,
+      image: `\\images\\${sku}.png`,
+    });
+    return true;
+  } catch (error) {
+    let message = `Error happend inserting product ${sku}`;
+    if (error instanceof Error) message = error.message;
+    throw new Error();
+  }
+};
