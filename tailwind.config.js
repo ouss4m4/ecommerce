f7b4c355b9401d77f5c7bc5a50d19d9f5832d761
Oussama <oussama@notifyai.io>
2024-12-26 11:03:25 +0400

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/public/**/*.html'], // Ensure path is relative to root
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
