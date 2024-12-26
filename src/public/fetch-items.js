67b2158b709820b95b77b0c39aa0bc3016637f90
ouss <bz.oussama@gmail.com>
2024-12-26 12:44:29 +0400

    restrict /products getAll() to page size default

--- src/public/fetch-items.js
+++ src/public/fetch-items.js
@@ -1,7 +1,12 @@
 const itemsWrap = document.getElementById('products');
 
 const getItemsList = async () => {
-  return await fetch('http://localhost:3001/api/v1/products').then((res) => res.json());
+  const urlParams = new URLSearchParams(window.location.search);
+  const page = urlParams.get('page');
+  const pageSize = 10; // get item from paginator object later
+  console.log(page);
+  const apiUrl = page ? `http://localhost:3001/api/v1/products?page=${page}&pageSize=${pageSize}` : 'http://localhost:3001/api/v1/products';
+  return await fetch(apiUrl).then((res) => res.json());
 };
 
 const createItemCard = (item) => {
