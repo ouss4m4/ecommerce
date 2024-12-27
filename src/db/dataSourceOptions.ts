c7d31d2982433d6869ffee5333af0bbc9a213fdc
Oussama <oussama@notifyai.io>
2024-12-27 12:09:35 +0400

    add migrations scripts

--- src/db/dataSourceOptions.ts
+++ src/db/dataSourceOptions.ts
@@ -1,18 +1,17 @@
-import { DataSourceOptions } from "typeorm";
-import { config } from "dotenv";
-import { Product } from "../entities/product.entity";
+import { DataSourceOptions } from 'typeorm';
+import { config } from 'dotenv';
 
 config();
 
 export const dataSourceOptions: DataSourceOptions = {
-  type: "postgres",
+  type: 'postgres',
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
   username: process.env.DB_USERNAME,
   password: `${process.env.DB_PASSWORD}`,
   database: process.env.DB_NAME,
-  entities: [Product],
-  migrations: ["dist/db/migrations/*.js"],
-  synchronize: true,
+  entities: ['dist/**/*.entity.js'],
+  migrations: ['dist/db/migrations/*.js'],
+  synchronize: false,
   logging: false,
 };
---
+++ src/entities/category.entity.ts
@@ -0,0 +1,19 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity()
+export class Category {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  slug: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
