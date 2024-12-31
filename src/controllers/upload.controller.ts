c99197fc54680bee68779a0b5c2a19f1bc7ae326
ouss <bz.oussama@gmail.com>
2024-12-31 18:44:46 +0400

    handle mass insert error and swtich to onebyone

--- src/controllers/upload.controller.ts
+++ src/controllers/upload.controller.ts
@@ -7,6 +7,7 @@ import { batchInsertProducts } from '../lib/batchInsertProducts';
 export class ProductBatchUploadController {
   //   static async handle(req: Request, res: Response): Promise<Response<IUploadResponse>> {
   static async handle(req: Request, res: Response): Promise<any> {
+    const batchSize = 10;
     if (!req.file) {
       return res.json({
         success: false,
@@ -37,26 +38,33 @@ export class ProductBatchUploadController {
     res.write(JSON.stringify(uploadResult));
 
     // process rows in batches of 10
-    let rowsBatch = [];
+    let rowsBatch: any[] = [];
     let count = 0;
+
+    const processBatch = async () => {
+      // update progress
+      uploadResult.message = `${count} rows in progress`;
+      res.write(JSON.stringify(uploadResult));
+
+      const { success: imgSuccess, errors: imgErrors } = await batchDownloadImages(rowsBatch);
+
+      uploadResult.errors = uploadResult.errors.concat(imgErrors);
+      const { success: rowsInserted, errors: insertErrors } = await batchInsertProducts(imgSuccess);
+      console.log({ rowsInserted, insertErrors });
+      count += rowsInserted;
+      uploadResult.errors = uploadResult.errors.concat(insertErrors);
+    };
 
     for await (const row of csvStream) {
       rowsBatch.push(row);
-      ++count;
-      if (rowsBatch.length == 5) {
-        // update progress
-        uploadResult.message = `${count} rows in progress`;
-        res.write(JSON.stringify(uploadResult));
-
-        const { success: imgSuccess, errors: imgErrors } = await batchDownloadImages(rowsBatch);
-
-        uploadResult.errors = uploadResult.errors.concat(imgErrors);
-        const { success: insertSuccess, errors: insertErrors } = await batchInsertProducts(imgSuccess);
-
-        uploadResult.errors = uploadResult.errors.concat(insertErrors);
+      if (rowsBatch.length == batchSize) {
+        await processBatch();
         rowsBatch = [];
       }
     }
-    res.end(JSON.stringify({ ...uploadResult, message: `Processed ${count - uploadResult.errors.length} successfully` }));
+    if (rowsBatch.length) {
+      await processBatch();
+    }
+    res.end(JSON.stringify({ ...uploadResult, message: `Processed ${count} successfully` }));
   }
 }
--- src/lib/batchInsertProducts.ts
+++ src/lib/batchInsertProducts.ts
@@ -3,24 +3,39 @@ import { IBatchProductInsertResponse, IProductDTO } from '../types/api';
 import { AppDataSource } from '../db';
 import { Product } from '../entities/product.entity';
 
+const productRepo = AppDataSource.getRepository(Product);
+
 export const batchInsertProducts = async (products: IProductDTO[]): Promise<IBatchProductInsertResponse> => {
-  let response: IBatchProductInsertResponse = {
-    success: true,
+  const response: IBatchProductInsertResponse = {
+    success: 0,
     errors: [],
   };
-
-  const productRepo = AppDataSource.getRepository(Product);
   try {
-    await productRepo.insert(products);
-    response.success = true;
+    // await productRepo.insert(products);
+    await productRepo.upsert(products, {
+      conflictPaths: ['sku'],
+    });
+    response.success = products.length;
     return response;
   } catch (error) {
-    // TODO: if mass insert failts. default to one by one. and show the errored one
-    let message = 'insert failed for product';
-    if (error instanceof Error) {
-      message = error.message;
-    }
-    response.errors.push(message);
-    return response;
+    return await insertOneByOne(products);
   }
+};
+
+const insertOneByOne = async (products: IProductDTO[]): Promise<IBatchProductInsertResponse> => {
+  const response: IBatchProductInsertResponse = {
+    success: 0,
+    errors: [],
+  };
+  for (let i = 0; i < products.length; i++) {
+    const product = products[i];
+    try {
+      await productRepo.upsert(product, { conflictPaths: ['sku'] });
+      response.success += 1;
+    } catch (err: any) {
+      response.errors.push(`${err?.message ?? 'failed to insert in db '} | ${product.sku}`);
+    }
+  }
+
+  return response;
 };
